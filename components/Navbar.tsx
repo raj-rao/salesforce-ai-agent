333071d848ed104586c782e61d512906677f0fca
raj-rao <rajrao104@gmail.com>
2026-06-07 21:09:05 -0400

    Add active tab highlighting to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,33 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const navClass = (path: string) =>
+    pathname === path
+      ? "border-b-2 border-blue-600 pb-1 font-semibold text-blue-600"
+      : "pb-1 text-gray-600 hover:text-blue-600";
+
   return (
-    <nav className="border-b bg-white px-6 py-4">
-      <div className="flex gap-6">
-        <Link href="/">Home</Link>
+    <nav className="flex gap-8 border-b p-4">
+      <Link href="/" className={navClass("/")}>
+        Home
+      </Link>
 
-        {/*<Link href="/dashboard">
-          Dashboard
-        </Link>*/}
+      <Link href="/tab1" className={navClass("/tab1")}>
+        Tab 1
+      </Link>
 
-        <Link href="/tab1">
-          Tab 1
-        </Link>
+      <Link href="/tab2" className={navClass("/tab2")}>
+        Tab 2
+      </Link>
 
-        <Link href="/tab2">
-          Tab 2
-        </Link>
-
-        <Link href="/tab3">
-          Tab 3
-        </Link>
-      </div>
+      <Link href="/tab3" className={navClass("/tab3")}>
+        Tab 3
+      </Link>
     </nav>
   );
 }
